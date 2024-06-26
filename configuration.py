b35a95d458e84d705c463720292c9b7c397da181
RavenRipper <152418271+RavenRipper@users.noreply.github.com>
2024-06-26 14:39:22 +0600

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,19 @@
+#Task 1
+URL_1 =  "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+
+First_name = "Иван"
+Last_name = "Петров"
+Address = "Ленина, 55-3"
+Email = "test@skypro.com"
+Phone_number = "+7985899998787"
+Zip_code = ""
+City = "Москва"
+Country = "Россия" 
+Job_position = "QA"
+Company = "SkyPro"
+
+#Task 2
+URL_2 =  "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+
+#Task 3
+URL_3 = "https://www.saucedemo.com/"
